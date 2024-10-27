ac6ec336dfa575fa9555724e4f85d10736e6a635
fengjutian <2640111081@qq.com>
2024-10-27 14:28:53 +0800

    save

---
+++ people_info/FunFactsView.swift
@@ -0,0 +1,37 @@
+//
+//  FunFactsView.swift
+//  people_info
+//
+//  Created by Jutian Feng on 2024/10/27.
+//
+
+
+import SwiftUI
+
+struct FunFactsView: View {
+
+    @State private var funFact = ""
+    var body: some View {
+        VStack {
+            Text("Fun Facts")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                        
+            Text(funFact)
+                .padding()
+                .font(.title)
+                .frame(minHeight: 400)
+
+            Button("Show Random Fact") {
+                funFact = information.funFacts.randomElement()!
+            }
+        }
+        .padding()
+    }
+}
+
+struct FunFactsView_Previews: PreviewProvider {
+    static var previews: some View {
+        FunFactsView()
+    }
+}
---
+++ people_info/HomeView.swift
@@ -0,0 +1,36 @@
+//
+//  HomeView.swift
+//  people_info
+//
+//  Created by Jutian Feng on 2024/10/27.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    
+    var body: some View {
+        VStack {
+            Text("All About")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .padding()
+
+            Image(information.image)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .cornerRadius(10)
+                .padding(40)
+
+            Text(information.name)
+                .font(.title)
+        }
+    }
+    
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
